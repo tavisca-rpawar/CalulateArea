426e9a7f863bc9eee723c8dc9975b2eab7757fc2
Pawar <rupawar@internal.tavisca.com>
2019-07-22 23:45:53 +0530

    Add project files.

---
+++ CalulateArea/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalulateArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
